16d9e002790ce23932264564e2a4d9a93a02041f
mattyw <gh@mattyw.net>
2017-05-18 23:17:36 +0100

    main: Simple repl

--- src/Main.hs
+++ src/Main.hs
@@ -3,4 +3,15 @@ module Main where
 import Model.Model
 
 main :: IO ()
-main = putStrLn $ show $ run [] $ parse $ words "5 6 + 7 8 + *"
+main = repl []
+
+repl :: [Int] -> IO()
+repl stk = do
+    putStr "> "
+    line <- getLine
+    if line == ":q"
+        then putStrLn $ show stk
+        else do
+            let newStk = run stk (parse [line])
+            putStrLn $ show newStk
+            repl newStk
